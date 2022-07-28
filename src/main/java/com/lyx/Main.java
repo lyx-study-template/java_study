6638c5b7c2307611ac4d58de6dbe1446a209a049
李艳兴 <liyanxing@zhongruigroup.com>
2022-07-28 11:01:55 +0800

    创建完成

---
+++ src/main/java/com/lyx/Main.java
@@ -0,0 +1,11 @@
+package com.lyx;
+
+import cn.hutool.core.lang.Console;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Console.log("打印数据：{}", 123); // TODO 删除 李艳兴
+    }
+}
